7850a070517a09a22ab022ec85180f28f1a9107e
Stefano B <stefano.ballabeni+github@gmail.com>
2012-11-06 12:04:03 -0500

    test

---
+++ scripts/3d50d380.app.js
@@ -0,0 +1,26 @@
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          window.oRequestAnimationFrame      ||
+          window.msRequestAnimationFrame     ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+$(function () {
+  var $countdown = $('#countdown');
+  var until = new Date(2013, 2, 2, 8, 25);
+  var timespan = null;
+
+  var update = function () {
+    timespan = countdown(until, null, countdown.DEFAULTS);
+    $countdown.html(timespan.toHTML());
+  };
+
+  (function loop(){
+    requestAnimFrame(loop);
+    update();
+  })();
+});
